7a41e1ba0dfe40e5629b8fbdccaa58fe30ab0270
RadhikaVijay1617 <vradhu2006@gmail.com>
2025-02-12 16:29:25 -0600

    Created VariableDeclaration.java file

---
+++ src/Basics/VariableDeclaration.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class VariableDeclaration {
+
+    public static void main(String[] args){
+        int X = 10;
+        int Y = 20;
+        int Z = X+Y;
+        String FirstName = "Radhika";
+        String LastName = "Vijay";
+        System.out.println(X+Y);
+        System.out.println("FullName:  "+ FirstName+LastName);
+    }
+}
